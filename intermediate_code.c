ec6eb614f543897253f3f65142e3c4f89241b56a
sudoboi <guyphysicist@gmail.com>
2022-04-12 14:31:45 +0530

    stmts IR

--- intermediate_code.c
+++ intermediate_code.c
@@ -121,6 +121,9 @@ void IR_for_astnode(ast_node *root, tupleList *list)
     {
         IR_factor(root);
     }
+    else if (root->construct == thenStmts_ || root->construct == elsePart_ || root->construct == otherStmts_ || root->construct == stmts_) {
+        IR_stmts(root);
+    }
     // else if (root->construct == termPrime_)
     // {
     //     IR_termprime(root, list);
@@ -542,4 +545,27 @@ void IR_iterative(ast_node *root)
 
     root->list->head = t1;
     root->list->tail = t5;
+}
+
+// handling thenStmts_ and elsePart_ ,otherStmts_ and stmts_
+void IR_stmts(ast_node* root) {
+    if (root->firstChild == NULL)
+    {
+        root->place = NULL;
+        root->list = NULL;
+        return;
+    }
+    // first child ast_node
+    ast_node* curr = root->firstChild;
+    root->list->head = curr->list->head;
+    tupleList* tl = curr->list;
+
+    while(curr->nextSib != NULL) {
+        tl = curr->list;
+        ast_node* next = curr->nextSib;
+        tupleList* next_tl = next->list;
+        tl->tail = next_tl->head;
+        curr = curr->nextSib;
+    }
+    tl->tail = NULL;
 }
