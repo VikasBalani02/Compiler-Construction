b10ea4eb47b63a2884682cf260fec3e5234fe9ac
sudoboi <guyphysicist@gmail.com>
2022-04-15 11:04:19 +0530

    codegen init

---
+++ codegen.h
@@ -0,0 +1,23 @@
+char* op_map[] = {
+    "MUL",
+    "DIV",
+    "PLUS",
+    "MINUS",
+    "IF",
+    "GOTO",
+    "LT",
+    "LE",
+    "EQ",
+    "NE",
+    "GT",
+    "GE",
+    "NOT",
+    "LABEL",
+    "UNSET",
+    "SET",
+    "ASSIGN",
+    "READ",
+    "WRITE",
+    "FUNCT",
+    "ENDFUNCT"
+}
